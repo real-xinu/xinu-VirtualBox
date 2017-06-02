3df2056f7e2c321afa299563d99ee2c26f661953
Rajas H. Karandikar <rkarandi@purdue.edu>
2017-06-02 12:15:50 -0900

    Added file usbepinit.c

---
+++ device/usb/usbepinit.c
@@ -0,0 +1,22 @@
+/* usbepinit.c - usbepinit */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ * usbepinit  -  Initialize USB endpoint device
+ *------------------------------------------------------------------------
+ */
+devcall	usbepinit (
+		struct	dentry *devptr	/* Entry in device switch table	*/
+		)
+{
+	struct	usbepcblk *epcptr;	/* Endpoint control block	*/
+
+	epcptr = &usbeptab[devptr->dvminor];
+
+	epcptr->state = USBEP_STATE_FREE;
+	epcptr->usbdptr = NULL;
+	epcptr->epptr = NULL;
+
+	return OK;
+}
